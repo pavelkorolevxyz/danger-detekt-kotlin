8a35c1935c003c88790049a2e251612579ef91ad
Pavel Korolev <pavelkorolevxyz@gmail.com>
2022-12-15 18:54:47 +0600

    Dependencies versions updated

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     base
-    id("io.gitlab.arturbosch.detekt").version("1.20.0")
+    id("io.gitlab.arturbosch.detekt").version("1.22.0")
 }
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -10,9 +10,9 @@ version = "1.1.0"
 
 dependencies {
     implementation("systems.danger:danger-kotlin-sdk:1.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.14.0")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
 }
